2858fd64c3db16b5a10d5820f949c2fda0fea0ac
Gaia <g.miglino@student.alfa-college.nl>
2020-01-16 11:46:42 +0100

    update android app

--- applicaties/android app/app/src/main/java/com/example/stagesysteem/MainActivity.java
+++ applicaties/android app/app/src/main/java/com/example/stagesysteem/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.stagesysteem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ImageView Background = this.findViewById(R.id.background);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
