4a3700734e8f2acdec450a9f8c45527a25c1f520
Marvin <m.koning02@student.alfa-college.nl>
2020-01-20 11:45:36 +0100

    Izabellas weblayout

--- applicaties/stagesysteemwebsite/src/App.js
+++ applicaties/stagesysteemwebsite/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
@@ -8,19 +8,37 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          StageSyteem
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body className="App-body"> 
+     <h3>Login om je ideale stage of leerbaan te vinden!</h3>
+     <br></br>
+     <br></br>
+     
+     <form>
+        <label>
+         Email
+         <br></br>
+         <input type="text" name="name" />
+        </label>
+        <br></br>
+        <br></br>
+        <label>
+         Password 
+         <br></br>
+         <input type="text" name="name" />
+        </label>
+        <br></br>
+        <br></br>
+        <input className="submit" type="submit" value="Login"/>
+      </form>
+    </body>
     </div>
+  
   );
 }
 
 export default App;
+
